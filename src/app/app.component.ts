a3b6ba7dbe7adab5c447fc90aea1ff553ab3edb2
motta1616 <ingeniero.leonardomotta@gmail.com>
2021-04-13 17:57:03 -0500

    Se crea el setup y se modifica el componente de html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
-@Component({
+import {AngularFirestore} from '@angular/fire/firestore'// cambiar
+import { Observable } from 'rxjs'; // cambio
+@Component({ // es el modelo vista controlador 
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html', // es nuestro temple donde ponemos nuestra vista 
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-chat-motta';
+  items: Observable<any[]>;
+  constructor(firestore: AngularFirestore) {
+    this.items = firestore.collection('messages').valueChanges();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import {AngularFireModule} from '@angular/fire'
 
 import { AppComponent } from './app.component';
 
+
+
+const firebase = {
+  apiKey: "AIzaSyAjSmXTsJwbdM2dWiFJCKqFfBXWkwSn6DM",
+  authDomain: "sofkachat-motta.firebaseapp.com",
+  projectId: "sofkachat-motta",
+  storageBucket: "sofkachat-motta.appspot.com",
+  messagingSenderId: "661924347522",
+  appId: "1:661924347522:web:16296cc65f8d5d328dfe9b"
+};
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
